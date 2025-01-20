5b2a3a190ca381ff69382fc988e499bd42e4052f
Ishtiak Saad <ishtiak.m.saad@gmail.com>
2025-01-21 00:55:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.gkupt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const database = client.db("Foodbank");
+        const foodCollection = database.collection("foods");
+        const userCollection = database.collection("users");
+
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// Default Route
+app.get('/', (req, res) => {
+    res.send('Food is falling from the Sky');
+});
+
+// Start the Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
